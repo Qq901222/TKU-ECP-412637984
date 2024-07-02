044a2dfe1f9102f7c280243ffff3ec93f16da5c0
Qq901222 <smaggie50038@gmail.com>
2024-07-02 11:55:50 +0800

    Add files via upload

---
+++ sim.java
@@ -0,0 +1,10 @@
+public class sim {
+    public static void main(String[]args){
+        int num1=2;
+        int num2=3;
+        System.out.println(num1+"cats are running");
+        System.out.println(num2+"dogs are running");
+    
+    }
+    
+}
