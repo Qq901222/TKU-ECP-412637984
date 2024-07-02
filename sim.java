57006f48ec6d1c24d20daa8c14dee8d4bd0f1e6c
Qq901222 <smaggie50038@gmail.com>
2024-07-02 12:01:10 +0800

    Add files via upload

--- sim.java
+++ sim.java
@@ -1,10 +1,16 @@
-public class sim {
-    public static void main(String[]args){
-        int num1=2;
-        int num2=3;
-        System.out.println(num1+"cats are running");
-        System.out.println(num2+"dogs are running");
-    
+//Ch2 簡單的Java程式
+
+public class sim {          //定義Public類別
+    public static void main(String[]args){          //main函數，主程式開始
+        int num1=2;  //宣告整數num
+        int num2=3;  //宣告整數num
+        float num3=4;
+        String num4="五";
+       
+        System.out.println(num1+"cats are running");        //印出字串及變數內容
+        System.out.println(num2+"dogs are running");        //印出字串及變數內容
+        System.out.println(num3+"birds are running");
+        System.out.println(num4+"birds are running");
     }
     
 }
---
+++ simple.java
@@ -0,0 +1,9 @@
+/**
+ * 0702
+ */
+public class simple{
+    public static void main(String arge[]){
+     System.out.println("Hello java!");
+     System.out.println("0702");
+    }
+}
