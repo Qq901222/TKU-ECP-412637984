0ad857d06844cc92fc9415ec8120f98357c01970
Qq901222 <smaggie50038@gmail.com>
2024-07-03 11:01:23 +0800

    Add files via upload

---
+++ CH2_2.java
@@ -0,0 +1,10 @@
+public class CH2_2 {    //定類別CH2_2
+    public static void main(String[]arge){
+        int a;          //宣告變數a，但不設定初值
+int b=4, sum;           //宣告b和sum兩個變數，但只將b的值設為4
+
+a=5;        //設定a的值=5
+sum=a+b;    //將sum的值設為a+b
+System.out.println("sum="+sum);         //印出sum值
+    }
+}
---
+++ CH2_3.java
@@ -0,0 +1,7 @@
+public class CH2_3 {
+   public static void main(String[]arges){
+    int a=2,b=5;
+
+    System.out.println(a+"*"+b+"="+(a*b));
+   }
+}
